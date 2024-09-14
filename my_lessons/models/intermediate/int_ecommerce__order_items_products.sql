ad1d07c8b7f63d99a890fccbce549f2d9f6480e8
Your Name <you@example.com>
2024-09-14 17:17:24 +0300

    added mart layer and fixed bug with num orders

--- my_lessons/models/intermediate/int_ecommerce__order_items_products.sql
+++ my_lessons/models/intermediate/int_ecommerce__order_items_products.sql
@@ -1,26 +1,30 @@
-WITH source AS (SELECT product_id,
-                       department   as product_department,
-                       cost         as product_cost,
-                       retail_price as product_retail_price,
+WITH products AS (
+	SELECT
+		product_id,
+		department AS product_department,
+		cost AS product_cost,
+		retail_price AS product_retail_price
 
-                FROM {{ ref('stg_ecommerece__products') }})
+	FROM {{ ref('stg_ecommerce__products') }}
+)
 
 SELECT
-    -- IDs
-    order_items.order_item_id,
-    order_items.order_id,
-    order_items.user_id,
-    order_items.product_id,
 
-    -- Order item data
-    order_items.item_sale_price,
+	-- IDs
+	order_items.order_item_id,
+	order_items.order_id,
+	order_items.user_id,
+	order_items.product_id,
 
-    -- Product data
-    products.product_department,
-    products.product_cost,
-    products.product_retail_price,
+	-- Order item data
+	order_items.item_sale_price,
 
-    -- Calculated fields
+	-- Product data
+	products.product_department,
+	products.product_cost,
+	products.product_retail_price,
+
+	-- Calculated fields
 	order_items.item_sale_price - products.product_cost AS item_profit,
 	products.product_retail_price - order_items.item_sale_price AS item_discount
 
--- my_lessons/models/staging/stg_ecommerce__order_items.sql
+++ my_lessons/models/staging/stg_ecommerce__order_items.sql
@@ -1,20 +1,17 @@
 WITH source AS (
-        SELECT *
+	SELECT *
 
-        FROM {{ source('thelook_ecommerce', 'order_items') }}
+	FROM {{ source('thelook_ecommerce', 'order_items') }}
 )
 
 SELECT
-        id AS order_item_id,
-        order_id,
-        user_id,
-        product_id,
-        inventory_item_id,
-        status,
-        created_at,
-        shipped_at,
-        delivered_at,
-        returned_at,
-        sale_price
+	-- IDs
+	id AS order_item_id,
+	order_id,
+	user_id,
+	product_id,
+
+	-- Other columns
+	sale_price AS item_sale_price
 
 FROM source
